9796e7e4aa9c0caf2052ad2c9d8e939fbc027e94
vitalik203 <vzaluskiy23@gmail.com>
2024-11-11 11:47:56 +0200

    1

--- src/components/Main/GameBoard.jsx
+++ src/components/Main/GameBoard.jsx
@@ -7,7 +7,7 @@ export default function GameBoard({clickEvent, squareLook}) {
         {squareLook.map((el, rowIndex)=>
         <li key={rowIndex}>
           <ol>
-          {el.map((el, colIndex)=><li key={colIndex}><button onClick={() => clickEvent(rowIndex, colIndex)}>{el}</button></li>)}
+          {el.map((el, colIndex)=><li key={colIndex}><button disabled={el!==null} onClick={() => clickEvent(rowIndex, colIndex)}>{el}</button></li>)}
           </ol>
         </li>)}
     </ol>
